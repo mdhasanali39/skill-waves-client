1ffef0023d65a07da1ffa2d3dd7de662bc4f8286
mdhasanali39 <contact.hasankhanyt@gmail.com>
2023-11-09 17:19:51 +0600

    main.jsx file updated with authProvider wrap whole website content and queryClientProvider wrap whole web site , authProvider for pass context data anywhere in this web app and queryClientProvider for use tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,23 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/Router'
+import { Toaster } from 'react-hot-toast'
+import AuthProvider from './contexts/AuthProvider'
+
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>
     <RouterProvider router={router}></RouterProvider>
+    </AuthProvider>
+  </QueryClientProvider>
+    <Toaster/>
   </React.StrictMode>,
 )
